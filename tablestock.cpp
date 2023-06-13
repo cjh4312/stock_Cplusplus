9ff441cadb7e9560ed708034e446d39420d2765c
cjh4312 <cjh4312@gmail.com>
2023-06-13 16:43:14 +0800

    更新

--- tablestock.cpp
+++ tablestock.cpp
@@ -83,7 +83,7 @@ void TableStock::initTableView()
     QHeaderView *header=myStockView->verticalHeader();
     //拖拽交换行
     header->setSectionsMovable(true);
-    connect(header,&QHeaderView::sectionMoved,this,[=](int logicIndex,int oldIndex,int newIndex){
+    connect(header,&QHeaderView::sectionMoved,this,[=](int /*logicIndex*/,int oldIndex,int newIndex){
 //        QStringList s=GlobalVar::settings->value("myStock").toStringList();
 //        QString str=s[oldIndex];
 //        s.removeAt(oldIndex);
@@ -92,16 +92,17 @@ void TableStock::initTableView()
         StockInfo info=GlobalVar::mMyStockList.at(oldIndex);
         GlobalVar::mMyStockList.remove(oldIndex);
         GlobalVar::mMyStockList.insert(newIndex,info);
-//        int curIndex=myStockView->currentIndex().row();
-//        m_myStockModel->setModelData(GlobalVar::mMyStockList);
-//        myStockView->setModel(m_myStockModel);
-//        myStockView->setCurrentIndex(m_myStockModel->index(curIndex,0));
         QStringList s;
         for (int i=0;i<GlobalVar::mMyStockList.count();++i)
         {
             s<<GlobalVar::mMyStockList.at(i).code;
         }
         GlobalVar::settings->setValue("myStock",s);
+        QList<StockInfo> t;
+        m_myStockModel->setModelData(t);
+        m_myStockModel->setModelData(GlobalVar::mMyStockList);
+        myStockView->setModel(m_myStockModel);
+        myStockView->setCurrentIndex(m_myStockModel->index(newIndex,0));
     });
     QTableView *tl[3]={stockTableView,risingSpeedView,myStockView};
     for (int i=0;i<3;++i)
