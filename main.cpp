7c2a878712d33181c0ee58b23c20e35b5b655c7c
cjh4312 <cjh4312@gmail.com>
2024-10-13 11:51:18 +0800

    更新

--- main.cpp
+++ main.cpp
@@ -1,27 +1,6 @@
 #include "mainwindow.h"
-// #include <Windows.h>
-//#include <DbgHelp.h>
-//#include <QMessageBox>
 #include <QApplication>
 #include <QSharedMemory>
-
-//LONG ApplicationCrashHandler(EXCEPTION_POINTERS *pException)
-//{
-//    HANDLE hDumpFile = CreateFile(L"crash.dmp", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-//    if(hDumpFile != INVALID_HANDLE_VALUE)
-//    {
-//        MINIDUMP_EXCEPTION_INFORMATION dumpInfo;
-//        dumpInfo.ExceptionPointers = pException;
-//        dumpInfo.ThreadId = GetCurrentThreadId();
-//        dumpInfo.ClientPointers = TRUE;
-//        MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hDumpFile, MiniDumpNormal, &dumpInfo, NULL, NULL);
-//    }
-//    QMessageBox msgBox;
-//    msgBox.setText("application crash!");
-//    msgBox.exec();
-
-//    return EXCEPTION_EXECUTE_HANDLER;
-//}
 
 int main(int argc, char *argv[])
 {
@@ -32,10 +11,7 @@ int main(int argc, char *argv[])
         qApp->quit();
         return -1;
     }
-//    SetUnhandledExceptionFilter((LPTOP_LEVEL_EXCEPTION_FILTER)ApplicationCrashHandler);
-//    setvbuf(stdout, NULL, _IONBF, 0);
-//    setvbuf(stderr, NULL, _IONBF, 0);
-
+  
     MainWindow *w = new MainWindow ;
     a.installEventFilter(w);
     w->showMaximized();
