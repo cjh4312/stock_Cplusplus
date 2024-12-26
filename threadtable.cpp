da891a44c95692b782c807aa46cac23b279e0565
cjh4312 <cjh4312@gmail.com>
2024-12-26 15:24:19 +0800

    更新

--- threadtable.cpp
+++ threadtable.cpp
@@ -19,7 +19,6 @@ void ThreadTable::getTableData()
     if (GlobalVar::WhichInterface==1)
     {
         fs="m:0+t:6,m:0+t:80,m:1+t:2,m:1+t:23,m:0+t:81+s:2048";
-        // QString s="https://push2.eastmoney.com/api/qt/clist/get?pn=1&pz=6000&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=1&fid=f22&fs="+fs+"&fields=f2,f3,f5,f6,f7,f8,f9,f10,f11,f12,f14,f15,f16,f17,f18,f20,f21,f22,f23,f24,f25,f31,f32,f37,f38,f39,f40,f41,f45,f46,f48,f49,f57,f100,f102,f112,f113,f129,f135&_=1667954879297";
         QString s="https://push2.eastmoney.com/api/qt/clist/get?pn=1&pz=6000&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=1&fid=f22&fs="+fs+"&fields=f2,f3,f5,f6,f7,f8,f9,f10,f11,f12,f14,f15,f16,f17,f18,f20,f21,f22,f23,f24,f25,f31,f32&_=1667954879297";
         GlobalVar::getData(allData,3,QUrl(s));
 
